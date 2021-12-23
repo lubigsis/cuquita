7dc2a39accdd177a26c49dc730eb9c57ba23f44c
lubigsis <lucre.baluc@gmail.com>
2021-12-22 21:16:57 -0300

    agregué 2 nros aleatorios para que la galletita se mueva al tratar de agarrarla

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ let puntos = 0;
 
 function sumarPuntos(){
     puntos++;
-    document.getElementById("puntajeJS").innerHTML = puntos;
-}
+    document.getElementById("puntajeJS").innerHTML = "Puntos: <b>" + puntos + "</b>";
+
+    randomNum1 = Math.round(Math.random()*400);
+    randomNum2 = Math.round(Math.random()*400);
+
+    document.getElementById("playerJS").style.marginTop = randomNum1 + "px";
+    document.getElementById("playerJS").style.marginLeft = randomNum2 + "px";
+};
