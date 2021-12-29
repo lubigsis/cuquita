70f0e2667278edb630da897488464f687d767555
lubigsis <lucre.baluc@gmail.com>
2021-12-29 17:45:04 -0300

    agregué el tiempo restante y los 2 botones

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 document.getElementById('playerJS').addEventListener("mouseover", sumarPuntos);
 
 let puntos = 0;
+let tiempo = 60;
+let delTotal = 50;
+
 
 function sumarPuntos(){
     puntos++;
-    document.getElementById("puntajeJS").innerHTML = "Puntos: <b>" + puntos + "</b>";
+    document.getElementById("puntajeJS").innerHTML = "Puntos: <b>" + puntos + "/" + delTotal + "</b>";
 
     randomNum1 = Math.round(Math.random()*400);
     randomNum2 = Math.round(Math.random()*400);
 
     document.getElementById("playerJS").style.marginTop = randomNum1 + "px";
     document.getElementById("playerJS").style.marginLeft = randomNum2 + "px";
+
+    if (puntos == 50){
+        alert("ganaste");
+    }
 };
+
+function restarTiempo(){
+    tiempo--;
+    document.getElementById("cont-tiempoJS").innerHTML = "Tiempo: " + tiempo;
+    if (tiempo == 0){
+       alert("perdiste");
+        tiempo = 0;
+        puntos = 0;
+    }
+}
+
+setInterval(restarTiempo, 1000);
